003db8474275c631489a7df9c6c481f331eaaae9
Karol Acosta <karolacosta167@gmail.com>
2026-02-23 16:38:09 -0500

    Add files via upload

---
+++ js/barra.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("navbar").innerHTML = `
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Sabores del Campo</a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="menu.html">Menú</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contacto.html">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  `;
+});
---
+++ js/footer.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  document.getElementById("footer").innerHTML = `
+  
+  <footer class="bg-success text-white text-center py-4 mt-5">
+    <div class="container">
+
+      <h4 class="mb-4">Síguenos en redes sociales</h4>
+
+      <div class="mb-3">
+        
+        <a href="https://facebook.com" target="_blank" class="me-4">
+          <img src="imagenes/facebook-logo.png" width="30" alt="Facebook">
+        </a>
+
+        <a href="https://instagram.com" target="_blank" class="me-4">
+          <img src="imagenes/Instagram-Logo.png" width="50" alt="Instagram">
+        </a>
+
+        <a href="https://twitter.com" target="_blank" class="me-4">
+          <img src="imagenes/Twitter.png" width="50" alt="Twitter">
+        </a>
+
+      </div>
+
+      <p class="mb-0">© 2026 Sabores del Campo</p>
+
+    </div>
+  </footer>
+
+  `;
+
+});
